53248439ead5b9777196533e96e1912ca33c3b19
Kyle Bernstein <kbernst30@gmail.com>
2019-08-26 13:50:36 -0400

    Use setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+import os
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-data-seeder',
+    version='0.1.0',
+    packages=find_packages(),
+    include_package_data=True,
+    license='BSD-3-Clause',
+    description='A data seeder for models for Django.',
+    long_description=README,
+    author='Kyle Bernstein',
+    author_email='kbernst30@gmail.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 2.2',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
