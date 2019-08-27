31c9153954da7f21c280b2d874e2bae271f0f131
Kyle Bernstein <kbernst30@gmail.com>
2019-08-27 13:05:56 -0400

    Updated build to use CI for deployment

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
 import os
+import sys
+
 from setuptools import find_packages, setup
+from setuptools.command.install import install
+
+VERSION = '0.1.1'
 
 with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
     README = readme.read()
@@ -7,9 +12,26 @@ with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
+
+class VerifyVersionCommand(install):
+    """Custom command to verify that the git tag matches our version"""
+
+    description = 'verify that the git tag matches our version'
+
+    def run(self):
+        tag = os.getenv('CIRCLE_TAG')
+
+        if tag != VERSION and tag != 'v' + VERSION:
+            info = "Git tag: {0} does not match app version: {1}".format(
+                tag, VERSION
+            )
+
+            sys.exit(info)
+
+
 setup(
     name='django-data-seeder',
-    version='0.1.1',
+    version=VERSION,
     packages=find_packages(),
     include_package_data=True,
     license='BSD-3-Clause',
@@ -30,4 +52,7 @@ setup(
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
+    cmdclass={
+        'verify': VerifyVersionCommand,
+    },
 )
