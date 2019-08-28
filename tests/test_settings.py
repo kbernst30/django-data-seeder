a203cb3f1862c15245f7c459ea5f607da0afac56
Kyle Bernstein <kbernst30@gmail.com>
2019-08-28 13:05:28 -0400

    Added test suite, starting with base

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -1,4 +1,17 @@
+import os
+
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
 SECRET_KEY = 'fake-key'
+
 INSTALLED_APPS = [
     "tests",
 ]
+
+DATABASES = {
+    'default': {
+        'NAME': os.path.join(PROJECT_ROOT, 'db.sqlite'),
+        'ENGINE': 'django.db.backends.sqlite3',
+    }
+}
