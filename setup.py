715cdf4a9ef6388a399117cd7bae02f3ba5e8760
Kyle Bernstein <kbernst30@gmail.com>
2019-08-29 13:45:03 -0400

    New version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import sys
 from setuptools import find_packages, setup
 from setuptools.command.install import install
 
-VERSION = '0.1.2'
+VERSION = '0.2.0'
 
 with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
     README = readme.read()
