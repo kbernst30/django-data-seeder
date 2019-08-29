ae02c1bcefb3d6be6e2a08cef3926e2c1e4beec3
Kyle Bernstein <kbernst30@gmail.com>
2019-08-29 12:20:02 -0400

    Added docstrings for admin module

--- data_seeder/admin.py
+++ data_seeder/admin.py
@@ -1,3 +1,7 @@
+'''
+Classes and functions that are necessary for the Django Admin Site
+'''
+
 from django.contrib import admin
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
@@ -7,9 +11,43 @@ from .base import DataSeeder
 
 
 class DataGeneratorAdmin(admin.ModelAdmin):
+    '''
+    A subclass of django.contrib.admin.ModelAdmin
+
+    This class adds functionality to generate data seeds
+    right from the Django Admin site
+
+    Attributes
+    ----------
+
+    change_list_template : str
+        A path to the overriden change_list template
+
+
+    Methods
+    -------
+
+    get_urls : list
+        returns a list of valid URLs for the Admin site
+
+    generate : django.http.HttpResponse
+        returns a valid HttpResponse for generate action
+    '''
+
     change_list_template = "admin/data_generator/change_list.html"
 
     def get_urls(self):
+        '''
+        Returns a list of valid URLs for the Admin site
+
+        Returns
+        -------
+        list
+            a list of valid URLs for the Admin site. We add
+            the 'generate/' path to the URLs already valid
+            from the superclass
+        '''
+
         urls = super().get_urls()
         info = self.model._meta.app_label, self.model._meta.model_name
 
@@ -19,6 +57,30 @@ class DataGeneratorAdmin(admin.ModelAdmin):
         ] + urls
 
     def generate(self, request):
+        '''
+        Performs the generate action
+
+        If the action is a GET request, the function will process
+        and return a valid form to the user to generate seeds.
+
+        If the action is a POST request, this will properly process
+        the seeds, generate them, and redirect back to the change_list
+
+        Parameters
+        ----------
+
+        request : django.http.request
+            the request object
+
+
+        Returns
+        -------
+
+        django.http.HttpResponse
+            Either a response object containing the generate form and options
+            or a redirect response
+        '''
+
         if request.method == 'POST':
             num_to_generate = int(request.POST.get('generate_num', 1))
             DataSeeder(self.model, seeds=num_to_generate).seed()
@@ -35,8 +97,35 @@ class DataGeneratorAdmin(admin.ModelAdmin):
 
 
 def data_generator_register(Cls):
+    '''
+    A class decorator to register a ModelAdmin with the data
+    seeder for the Admin Site
+
+    Parameters
+    ----------
+
+    Cls : type
+        A type of django.contrib.admin.ModelAdmin
+
+
+    Returns
+    -------
+
+    type
+        type DataGeneratorRegisterdCls to wrap the original Cls
+    '''
 
     class DataGeneratorRegisterdCls(DataGeneratorAdmin):
+        '''
+        This class wraps a ModelAdmin so an instance of
+        that class gets the functionality of DataGeneratorAdmin
+
+        Attributes
+        ----------
+
+        oInstance : object
+            an instance of ModelAdmin to wrap
+        '''
 
         def __init__(self, *args, **kwargs):
             self.oInstance = Cls(*args, **kwargs)
